91995af9876aca39771051eae52d2bf6d21497c0
李禹辰 <liyuchen@wirelessprv-10-194-124-51.near.illinois.edu>
2018-03-29 10:58:06 -0500

    query to tweets

--- read_pickle.py
+++ read_pickle.py
@@ -3,6 +3,7 @@
 
 import codecs
 import pickle
+#from learner import search
 
 '''
 Read the pickle file containing the processed tweets data.
@@ -16,7 +17,27 @@ def read_pickle(fn):
 
 if __name__=="__main__":
     
+    # example
     # read the pickle file containing the processed tweets data.
     # t is a list of tweets object.
-    fn = 'data/tweets1100000_tokenized.pickle'
+    '''
+    fn = 'data/tweets1100000_tokenized_group/tweets1100000_tokenized_0.pickle'
     t = read_pickle(fn)
+    '''
+    
+    # read inverted index # see search.py
+    '''
+    fn = 'learner/data/tweets1100000_partial_0_.pickle'
+    t = search.read_pickle(fn)
+    '''
+    
+    '''
+    2018.3.22
+    Create id_to_tweet_dict: a dictionary (tweetID -> Tweet object)
+    '''
+    #fn = 'data/tweets1100000_tokenized.pickle'
+    #t = read_pickle(fn)
+    
+    id_to_tweet_dict = {}
+     
+    
